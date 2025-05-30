e5c12c9865501ecd4d87071d8b7e0ddb9cc02a49
Lasssssa <theopodoro@gmail.com>
2025-05-30 21:31:24 +0200

    Fix API

--- api/src/test/index.ts
+++ api/src/test/index.ts
@@ -1,9 +1,8 @@
 import {createRouter} from "@/utils/create-router";
-import {authMiddlewareForRoles} from "@/middlewares/auth";
 
 const testRouter = createRouter();
 
-testRouter.get('/', authMiddlewareForRoles(['USER']), async function (req, res, next) {
+testRouter.get('/', async function (req, res, next) {
     res.status(200).json({message: 'Its a good test!'});
 });
 
